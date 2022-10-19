02776530f9c32a79609f760b33f23ff7b438d39a
Jiri Malec <jiri.malec@bluesoft.cz>
2022-10-19 18:16:44 +0200

    !

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './output.css';
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <span className="text-3xl font-bold">
+          Hello tailwind
+        </span>
         <a
           className="App-link"
           href="https://reactjs.org"
