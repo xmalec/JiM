1789c98fc15672846351e79e11b644dffa6ef1a6
Jiří Malec <jirka.malec98@seznam.cz>
2022-10-29 21:13:25 +0200

    Implementing repository

---
+++ WebApi/src/DAL/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        public void Insert(TEntity entity);
+        public void Update(TEntity entity);
+        public void Delete(TEntity entity);
+        public void Delete(int id);
+        public TEntity Find(int id);
+    }
+}
---
+++ WebApi/src/DAL/Repositories/Repository.cs
@@ -0,0 +1,55 @@
+﻿using DAL.Data;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+
+        private readonly DALDbContext context;
+        private readonly DbSet<TEntity> dbSet;
+
+        public Repository(DALDbContext dbContext)
+        {
+            this.context = dbContext;
+            dbSet = dbContext.Set<TEntity>();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            if (context.Entry(entity).State == EntityState.Detached)
+            {
+                dbSet.Attach(entity);
+            }
+            dbSet.Remove(entity);
+        }
+
+        public void Delete(int id)
+        {
+            TEntity entityToDelete = dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public TEntity Find(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            dbSet.Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
