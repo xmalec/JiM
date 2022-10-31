c3dc3ae2c4b2057d1471e21c5eb16f3cdf64a8b5
Jiri Malec <jiri.malec@bluesoft.cz>
2022-10-31 08:11:48 +0100

    init bussiness layer

--- WebApi/BL/Models/ProjectDto.cs
+++ WebApi/BL/Models/ProjectDto.cs
@@ -1,7 +1,8 @@
-namespace DAL.Models
+﻿namespace BL.Models
 {
-    public class Project : BaseEntity
+    public class ProjectDto
     {
+        public int Id { get; set; }
         public string ProjectName { get; set; }
         public string Slug { get; set; }
         public string Description { get; set; }
