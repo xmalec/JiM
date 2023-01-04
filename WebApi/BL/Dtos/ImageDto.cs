8c7f986472564d294e14be6022a9feeb6bd08c7c
Jiri Malec <jiri.malec@bluesoft.cz>
2023-01-04 17:11:45 +0100

    Creating unit of work global

---
+++ WebApi/BL/Dtos/ImageDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BL.Dtos
+{
+    public class ImageDto
+    {
+        public string FilePath { get; set; }
+        public string? Name { get; set; }
+        public string? Variant { get; set; }
+        public string? Alt { get; set; }
+    }
+}
