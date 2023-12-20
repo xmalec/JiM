c1c3d3c1c549c7abfa26567133eb536e875cc19f
Jiri Malec <jiri.malec@bluesoft.cz>
2023-12-20 09:48:17 +0100

    wip

--- WebApi/src/BL/Services/Email/IEmailService.cs
+++ WebApi/src/BL/Services/Email/IEmailService.cs
@@ -10,5 +10,6 @@ namespace BL.Services.Email
     public interface IEmailService : IService
     {
         Task SendEmailAsync(EmailRequest mailRequest);
+        Task SendNewUserEmail(NewUserModel model, bool awaitEmailSend = false);
     }
 }
