37667d80412dc8d1b94c874197c10ecf948ad570
Jiří Malec <jirka.malec98@seznam.cz>
2023-12-21 18:17:59 +0100

    service registration

---
+++ WebApi/src/BL/Constants/ApplicationSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Constants
+{
+    public class ApplicationSettings
+    {
+        private const string PREFIX = "ApplicationSettings";
+        public static string BrandName => $"{PREFIX}.BrandName";
+        public static string HelpEmail => $"{PREFIX}.HelpEmail";
+    }
+}
---
+++ WebApi/src/BL/Services/Setting/SettingService.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BL.Services.Setting
+{
+    public class SettingService : ISettingService
+    {
+        private readonly IConfiguration configuration;
+        private readonly ILogger<SettingService> logger;
+
+        public SettingService(IConfiguration configuration, ILogger<SettingService> logger)
+        {
+            this.configuration = configuration;
+            this.logger = logger;
+        }
+
+        public bool GetBoolean(string key)
+        {
+            if (Validate(key, out var value))
+            {
+                if (bool.TryParse(value, out var result))
+                {
+                    return result;
+                }
+                logger.LogError("Cannot get boolean value of setting {0}! Application setting is not an boolean.", key);
+                throw new ArgumentException("Cannot get boolean value of setting {0}! Application setting is not an boolean.", key);
+            }
+            return default;
+        }
+
+        public string GetString(string key)
+        {
+            if (Validate(key, out var value))
+            {
+                return value;
+            }
+            return string.Empty;
+        }
+
+        public int GetInteger(string key)
+        {
+            if(Validate(key, out var value))
+            {
+                if (int.TryParse(value, out var result))
+                {
+                    return result;
+                }
+                logger.LogError("Cannot get integer value of setting {0}! Application setting is not an integer.", key);
+                throw new ArgumentException("Cannot get integer value of setting {0}! Application setting is not an integer.", key);
+            }
+            return default;
+        }
+
+        private bool Validate(string key, out string value)
+        {
+            if(string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentException("Invalid key.");
+            }
+            if (!GetSetting(key, out value))
+            {
+                logger.LogError("Application setting {0} is not set!", key);
+                throw new ArgumentException("Application setting {0} is not set!", key);
+            }
+            return true;
+        }
+
+        private bool GetSetting(string key, out string res)
+        {
+            res = configuration[key] ?? string.Empty;
+            return !string.IsNullOrEmpty(res);
+        }
+
+        
+    }
+}
